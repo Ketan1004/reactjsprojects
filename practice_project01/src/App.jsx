84e0e962bb0d4382f781925786153568318417c1
Ketan1004 <ashishshrikhande250@gmail.com>
2024-06-30 14:51:38 +0530

    done

--- practice_project01/src/App.jsx
+++ practice_project01/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 function App() {
+
+
+  function handleClick(){
+        let randomNum  = Math.floor(Math.random() * 3) + 1;
+        console.log(randomNum);
+        let userInput = prompt("type a number");
+        console.log(userInput);
+        alert(`Number: ${randomNum}, your Guess: ${userInput}`);
+  }
   return (
-    <div>App</div>
+    <div>
+     <h1> Task: Add a button and handle a click event</h1>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={handleClick}>Guess the Number Between 1 and 3</button>
+
+    </div>
   )
 }
 
